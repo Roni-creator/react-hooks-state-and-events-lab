ea135687ead977f398b5b3ce2d39c445bbd5d309
veronicahmbuvi <mbuviveronicah3@gmail.com>
2023-04-20 06:04:06 -0700

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [added, setIsOn] = useState(false);
+
+  function handlePush() {
+    setIsOn((added) => !added);
+  }
+
   return (
-    <li className="">
+    <li className={added ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={added ? "remove" : "add"} onClick={handlePush}>{added ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
 
 export default Item;
+
